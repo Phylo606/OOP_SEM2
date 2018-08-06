cbfc5bc7f6a5cf69383f7111eab9820c10849b19
Matthew Phyland <mphyland2015@gmail.com>
2018-08-06 15:06:25 +1000

    Week 2 Project

---
+++ PrimeAndFibonacci/PrimeAndFibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeAndFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total;
+            List<int> list = new List<int>();
+
+            while (true)
+            {
+                for int (p = 0, p < 0, p++)
+                {
+                    
+                }
+                for (int i = i, i < count, i++)
+                {
+                    if ( i = fib_num && prime_num)
+                    {
+                        Console.WriteLine(i + " is both a fibonacci number and a prime number");
+                    }
+                }
+            }
+        }
+    }
+}
