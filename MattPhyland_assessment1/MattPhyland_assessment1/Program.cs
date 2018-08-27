d80e1602dfbda07c035e3c209199d02046a70bb5
Matthew Phyland <mphyland2015@gmail.com>
2018-08-27 20:42:02 +1000

    init commit

---
+++ MattPhyland_assessment1/MattPhyland_assessment1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MattPhyland_assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square cubey = new Square(2, 2, 2, 2, "red");
+            
+
+            Console.WriteLine(cubey.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MattPhyland_assessment1/MattPhyland_assessment1/Shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MattPhyland_assessment1
+{
+    public abstract class Shape
+    {
+        public string Colour { get; set; }
+        public Shape(string colour)
+        {
+            this.Colour = colour;
+        }
+    }
+    public abstract class Quadrilateral : Shape
+    {
+        public int Side1Length { get; set; }
+        public int Side2Length { get; set; }
+        public int Side3Length { get; set; }
+        public int Side4Length { get; set; }
+        public float GetPerimeter()
+        {
+            return (Side1Length + Side2Length + Side3Length + Side4Length);
+        }
+        public Quadrilateral(int side1length, int side2length, int side3length, int side4length, string colour) : base(colour)
+        {
+            this.Side1Length = side1length;
+            this.Side2Length = side2length;
+            this.Side3Length = side3length;
+            this.Side4Length = side3length;
+        }
+    }
+    public class Square : Quadrilateral
+    {
+        public float GetArea()
+        {
+            return (Side1Length * Side1Length);
+        }
+        public Square(int side1length, int side2length, int side3length, int side4length, string colour) : base(side1length, side2length, side3length, side4length, colour)
+        {
+
+        }
+    }
+    public class Rectangle : Quadrilateral
+    {
+        public float GetArea()
+        {
+            return (Side1Length * Side2Length);
+        }
+        public Rectangle(int side1length, int side2length, int side3length, int side4length, string colour) : base(side1length, side2length, side3length, side4length, colour)
+        {
+
+        }
+    }
+}
